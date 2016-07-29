d853ee7ec101bcf6cfd85d083e7b81d98898ec01
Bussmeyer <thomas.bussmeyer@publicispixelpark.de>
2016-07-29 14:54:12 +0000

    Merge install into readme. Add audio setup

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # miek
-# miek
+## Installation
+    sudo apt-get update -y
+    sudo apt-get install -y git
+    cd ~
+    git clone https://github.com/Bussmeyer/miek.git
+    cd miek
+    ./install-requirements.sh
+
+## References
+@see https://learn.adafruit.com/playing-sounds-and-using-buttons-with-raspberry-pi/overview
--- install-requirements.sh
+++ install-requirements.sh
@@ -1,3 +1,10 @@
 sudo apt-get update -y
-sudo apt-get install -y mpd mpc python-dev python-rpi.gpio python-mpd python-pyudev
+sudo apt-get install -y alsa-utils mpg123 mpd mpc python-dev python-rpi.gpio python-mpd python-pyudev
 
+sudo modprobe snd_bcm2835
+sudo amixer cset numid=3 1
+
+echo "Prepare for reboot"
+echo "If you're using SSH, you'll have to reconnect after it boots."
+sleep 3
+sudo reboot
