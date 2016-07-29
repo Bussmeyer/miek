e3f1baaa38971338fe5d8d17db1b937bdf5dbde7
Bussmeyer <thomas.bussmeyer@publicispixelpark.de>
2016-07-29 14:44:15 +0000

    Add dir and init script

---
+++ init.sh
@@ -0,0 +1,7 @@
+sudo apt-get update -y
+sudo apt-get install -y git
+cd ~
+git clone https://github.com/Bussmeyer/miek.git
+cd miek
+./install-requirements.sh
+
---
+++ install-requirements.sh
@@ -0,0 +1,3 @@
+sudo apt-get update -y
+sudo apt-get install -y mpd mpc python-dev python-rpi.gpio python-mpd python-pyudev
+
